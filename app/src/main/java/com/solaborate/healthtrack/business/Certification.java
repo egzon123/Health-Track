d44e23db4af7b4aafe6e5e5bdbb544b1eabee65f
Egzon Berisha <=egzon.berisha.9696@gmail.com>
2020-01-29 00:23:12 +0000

    added basic Ihealth basic configuration

---
+++ app/src/main/java/com/solaborate/healthtrack/business/Certification.java
@@ -0,0 +1,36 @@
+package com.solaborate.healthtrack.business;
+
+import android.app.Application;
+
+import com.ec.easylibrary.utils.ToastUtils;
+import com.ihealth.communication.manager.iHealthDevicesManager;
+import com.solaborate.healthtrack.BaseApplication;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class Certification {
+
+    public boolean isPassCertification(){
+        boolean isPass = false;
+        try {
+            InputStream is = BaseApplication.instance().getAssets().open("com_solaborate_healthtrack_android.pem");
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+             isPass = iHealthDevicesManager.getInstance().sdkAuthWithLicense(buffer);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+       return isPass;
+    }
+
+    public void checkIfPass(){
+        if(isPassCertification() == false){
+            ToastUtils.showToast(BaseApplication.instance(),"Certification not passed");
+        }else {
+            System.out.println("--->> Certification passed <<---");
+        }
+    }
+}
